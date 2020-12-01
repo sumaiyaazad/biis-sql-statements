82e60f34190f94e2ab68c241ef98ca61a6ce6618
Taaha <azadsumaiya00@gmail.com>
2020-12-01 20:35:01 +0600

    file upload,file structure,admin,token all done

--- STUDENTS.sql
+++ STUDENTS.sql
@@ -14,8 +14,12 @@
 -- INSERT INTO STUDENTS VALUES ('1705048','Sumaiya Azad','CSE','18-19/2-2',12,'Chatri','Attached','01711332312','Bailey Road','Abul Kalam Azad','01717214181','taaha','1705048.jpg');
 -- 
 -- INSERT INTO STUDENTS VALUES ('1605011','Mir Mahathir','CSE','18-19/3-2',12,'Chatri','Attached','01711332312','Bailey Road','Kamrunnesa Mala','01711134045','taaha','1705048.jpg');
+declare
+begin
+insert_STUDENT ('1905050','Muntaka Muntasir','CSE','19-20/1-1',12,'Chatri','Attached','01711332312','Bailey Road','muntasir@gmail.com','Kamrunnesa Mala','01711134045','taaha','https://www.persofoto.com/images/vorher-nachher/passfoto-beispiel-nachher.jpg');
+INSERT_STUDENT('1505050','Muntaka Muntasir','CSE','18-19/4-2',12,'Chatri','Attached','01711332312','Bailey Road','taaha@gmail.com','Kamrunnesa Mala','01711134045','taaha','https://www.persofoto.com/images/vorher-nachher/passfoto-beispiel-nachher.jpg');
+end;
 
-
-INSERT INTO STUDENTS VALUES ('1505050','Muntaka Muntasir','CSE','18-19/4-2',12,'Chatri','Attached','01711332312','Bailey Road','Kamrunnesa Mala','01711134045','taaha','https://www.persofoto.com/images/vorher-nachher/passfoto-beispiel-nachher.jpg');
-
+INSERT INTO STUDENTS VALUES ('1505050','Muntaka Muntasir','CSE','18-19/4-2',12,'Chatri','Attached','01711332312','Bailey Road','taaha@gmail.com','Kamrunnesa Mala','01711134045','taaha','https://www.persofoto.com/images/vorher-nachher/passfoto-beispiel-nachher.jpg');
+INSERT INTO STUDENTS VALUES ('1905050','Muntaka Muntasir','CSE','19-20/1-1',12,'Chatri','Attached','01711332312','Bailey Road','muntasir@gmail.com','Kamrunnesa Mala','01711134045','taaha','https://www.persofoto.com/images/vorher-nachher/passfoto-beispiel-nachher.jpg');
 -- INSERT INTO STUDENTS VALUES ('1705050','Muntaka Muntasir','CSE','18-19/4-2',12,'Chatri','Attached','01711332312','Bailey Road','Kamrunnesa Mala','01711134045','taaha','https://www.persofoto.com/images/vorher-nachher/passfoto-beispiel-nachher.jpg');
